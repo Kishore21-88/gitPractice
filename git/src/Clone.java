356d4f9670a85dde964e074bf96e91b23fbbe391
N <kishore.chethu21@gmail.com>
2022-06-10 23:58:53 +0530

    second commit

--- git/src/Clone.java
+++ git/src/Clone.java
@@ -2,7 +2,11 @@
 public class Clone {
 
 	public static void main(String[] args) {
-System.out.println("hello");
+//System.out.println("hello");
+		for(int i=1;i<4;i++)
+		{
+			System.out.println("hi...");
+		}
 
 	}
 
